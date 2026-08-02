b346244fcf5780a4b7452b05208ef0b4658fe4d4
chepkwonyben04-prog <chepkwonyben04@gmail.com>
2026-08-02 12:42:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from bot import ChefChepkwonyBot
 
+
 def main():
-    print("👨🏾‍🍳 Chef Chepkwony AI Bot is starting...")
-    
+
     bot = ChefChepkwonyBot()
+
     bot.run()
 
+
 if __name__ == "__main__":
+
     main()
